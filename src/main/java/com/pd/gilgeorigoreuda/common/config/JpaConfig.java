387352108ff4feff53d9d48f4030ad4630d74a5f
Sudongk <aa88sd@naver.com>
2023-10-24 20:24:44 +0900

    feature/#11 codecov.yml 수정

---
+++ src/main/java/com/pd/gilgeorigoreuda/common/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.pd.gilgeorigoreuda.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
+
+
