fd616abea69154bf3f4559ce937048208d31db85
Sudongk <aa88sd@naver.com>
2023-10-25 11:48:19 +0900

    refactor/#14 User Store Entity 연관관계 매핑

--- src/main/java/com/pd/gilgeorigoreuda/store/domain/entity/Store.java
+++ src/main/java/com/pd/gilgeorigoreuda/store/domain/entity/Store.java
@@ -3,14 +3,19 @@ package com.pd.gilgeorigoreuda.store.domain.entity;
 import java.time.LocalDate;
 
 import com.pd.gilgeorigoreuda.common.entity.BaseTimeEntity;
+import com.pd.gilgeorigoreuda.user.domain.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -65,5 +70,9 @@ public class Store extends BaseTimeEntity {
 
 	@Column(name = "store_image", columnDefinition = "TEXT")
 	private String storeImage;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "fk_store_user"))
+	private User user;
 
 }
