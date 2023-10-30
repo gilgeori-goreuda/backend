5526555a1213a38e8a45045df7a7c24b504d1736
HeeHeeg <qmffndoden@naver.com>
2023-10-30 17:25:10 +0900

    #18-리뷰모아보기: default 최신순, 평점순, 좋아요순가능하도록 페이징으로 구현

--- src/main/java/com/pd/gilgeorigoreuda/review/repository/ReviewRepository.java
+++ src/main/java/com/pd/gilgeorigoreuda/review/repository/ReviewRepository.java
@@ -1,8 +1,16 @@
 package com.pd.gilgeorigoreuda.review.repository;
 
 import com.pd.gilgeorigoreuda.review.domain.entity.Review;
+import com.pd.gilgeorigoreuda.review.dto.response.ReviewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT DISTINCT r FROM Review r " +
+            "LEFT JOIN FETCH r.images ")
+    Page<ReviewResponse> findAllReviewsWithImagesOrderByRecent(Pageable pageable);
 
 }
