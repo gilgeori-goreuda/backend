b0dd209da7f54fc521a3f0aabf575380bbbaaf46
HeeHeeg <qmffndoden@naver.com>
2023-11-01 14:54:43 +0900

    feature/#34-comment-read 댓글 response수정

--- src/main/java/com/pd/gilgeorigoreuda/review/dto/response/ReviewCommentResponse.java
+++ src/main/java/com/pd/gilgeorigoreuda/review/dto/response/ReviewCommentResponse.java
@@ -3,11 +3,20 @@ package com.pd.gilgeorigoreuda.review.dto.response;
 import com.pd.gilgeorigoreuda.review.domain.entity.ReviewComment;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ReviewCommentResponse {
     private String content;
+    private String nickname;
+    private String profileImageUrl;
+    private LocalDateTime createdAt;
+
 
     public ReviewCommentResponse(ReviewComment reviewComment) {
         this.content = reviewComment.getContent();
+        this.nickname = reviewComment.getMember().getNickname();
+        this.profileImageUrl = reviewComment.getMember().getProfileImageUrl();
+        this.createdAt = reviewComment.getCreatedAt();
     }
 }
