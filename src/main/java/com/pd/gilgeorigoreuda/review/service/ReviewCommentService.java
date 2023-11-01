2bd05b243f4e716ecc7686fcb3d4867bc8d09d79
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-11-01 10:56:48 +0900

    @Transactinal 추가

--- src/main/java/com/pd/gilgeorigoreuda/review/service/ReviewCommentService.java
+++ src/main/java/com/pd/gilgeorigoreuda/review/service/ReviewCommentService.java
@@ -7,12 +7,16 @@ import com.pd.gilgeorigoreuda.review.dto.request.ReviewCommentRequest;
 import com.pd.gilgeorigoreuda.review.repository.ReviewCommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ReviewCommentService {
+    
     private final ReviewCommentRepository commentRepository;
 
+    @Transactional
     public void saveComment(Long reviewId, Long memberId , ReviewCommentRequest request) {
         ReviewComment comment = ReviewComment.builder()
                 .content(request.getContent())
@@ -21,4 +25,5 @@ public class ReviewCommentService {
                 .build();
         commentRepository.save(comment);
     }
+    
 }
