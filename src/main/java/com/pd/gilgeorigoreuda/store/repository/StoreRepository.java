eca8ae880dcaac82f2ef83a30c6fbeda20e60aa6
Sudongk <82927161+Sudongk@users.noreply.github.com>
2023-11-03 15:18:32 +0900

    conflict resolve

--- src/main/java/com/pd/gilgeorigoreuda/store/repository/StoreRepository.java
+++ src/main/java/com/pd/gilgeorigoreuda/store/repository/StoreRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.time.LocalDateTime;
 
 import com.pd.gilgeorigoreuda.store.domain.entity.Store;
 
@@ -22,5 +24,20 @@ public interface StoreRepository extends JpaRepository<Store, Long> {
 		+ "left join fetch s.member m "
 		+ "where s.id = :storeId")
 	Optional<Store> findStoreWithMember(@Param("storeId") Long storeId);
+
+	 @Query("SELECT s " +
+            "FROM Store s " +
+            "where s.createdAt >= :startDay and s.createdAt <= :endDay " +
+            "order by s.createdAt desc " +
+            "limit 10")
+    	List<Store> findAllByBetweenDay(@Param("startDay") LocalDateTime startDay,
+                                    	@Param("endDay") LocalDateTime endDay);
+
+	@Query("SELECT s " +
+            "FROM Store s " +
+            "ORDER BY ((0.713 * (s.averageRating / 5)) + (0.287 * (s.totalVisitCount * 0.01))) " +
+            "DESC " +
+            "limit 10")
+    	List<Store> findStoresByWeightedAverageRating();
 
 }
