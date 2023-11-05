51bb08f3b13c2333344300b8c4d04a7dcd90abe7
Sudongk <aa88sd@naver.com>
2023-11-05 20:57:10 +0900

    setting/#78 RestDocs 세팅

--- src/test/java/com/pd/gilgeorigoreuda/settings/ControllerTest.java
+++ src/test/java/com/pd/gilgeorigoreuda/settings/ControllerTest.java
@@ -3,22 +3,36 @@ package com.pd.gilgeorigoreuda.settings;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pd.gilgeorigoreuda.home.controller.HomeController;
 import com.pd.gilgeorigoreuda.home.service.HomeService;
+import com.pd.gilgeorigoreuda.settings.restdocs.RestDocsConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.pd.gilgeorigoreuda.review.controller.ReviewController;
 import com.pd.gilgeorigoreuda.store.controller.StoreController;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
-@AutoConfigureRestDocs
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
 @WebMvcTest({
 //	ReviewController.class,
 //	StoreController.class,
 	HomeController.class
 })
+@Import(RestDocsConfiguration.class)
+@ExtendWith(RestDocumentationExtension.class)
+@MockBean(JpaMetamodelMappingContext.class)
 @ActiveProfiles("test")
 public abstract class ControllerTest {
 
@@ -26,9 +40,21 @@ public abstract class ControllerTest {
 	protected MockMvc mockMvc;
 
 	@Autowired
+	protected RestDocumentationResultHandler restDocs;
+
+	@Autowired
 	protected ObjectMapper objectMapper;
 
-	@MockBean
-	protected HomeService homeService;
+	@BeforeEach
+	void setUp(
+			final WebApplicationContext context,
+			final RestDocumentationContextProvider restDocumentation
+	) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+				.apply(documentationConfiguration(restDocumentation))
+				.alwaysDo(restDocs)
+				.addFilters(new CharacterEncodingFilter("UTF-8", true))
+				.build();
+	}
 
 }
