03479ee8482dbfb70f66dcaf57d3c8bd5f829135
jun <dudwns1852@naver.com>
2023-11-07 17:19:00 +0900

    feature/#87 본인이 신고한 가게만 신고취소 가능하도록 만듬

--- src/main/java/com/pd/gilgeorigoreuda/store/controller/StoreReportController.java
+++ src/main/java/com/pd/gilgeorigoreuda/store/controller/StoreReportController.java
@@ -4,20 +4,39 @@ import com.pd.gilgeorigoreuda.store.dto.request.ReportCreateRequest;
 import com.pd.gilgeorigoreuda.store.service.StorePreferenceService;
 import com.pd.gilgeorigoreuda.store.service.StoreReportService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/store/{storeId}/report")
+@RequestMapping("/api/v1/reports")
 public class StoreReportController {
+
     private static final Long TEST_USER_ID = 2L;
 
     private final StoreReportService storeReportService;
 
-    @PostMapping
-    public void addStoreReport(@PathVariable Long storeId,
-                               @RequestBody ReportCreateRequest reportCreateRequest){
+    @PostMapping("/stores/{storeId}")
+    public ResponseEntity<Void> addStoreReport(
+            @PathVariable final Long storeId,
+            @RequestBody final ReportCreateRequest reportCreateRequest
+    ){
         storeReportService.addStoreReport(reportCreateRequest, storeId, TEST_USER_ID);
+
+        return ResponseEntity
+                .ok()
+                .build();
+    }
+
+    @DeleteMapping("/stores/{reportId}")
+    public ResponseEntity<Void> deleteStoreReport(
+            @PathVariable final Long reportId
+    ){
+        storeReportService.deleteReport(reportId, TEST_USER_ID);
+
+        return ResponseEntity
+                .noContent()
+                .build();
     }
 
 }
--- src/main/java/com/pd/gilgeorigoreuda/store/service/StoreReportService.java
+++ src/main/java/com/pd/gilgeorigoreuda/store/service/StoreReportService.java
@@ -1,20 +1,42 @@
 package com.pd.gilgeorigoreuda.store.service;
 
+import com.pd.gilgeorigoreuda.store.domain.entity.Store;
+import com.pd.gilgeorigoreuda.store.domain.entity.StoreReportHistory;
 import com.pd.gilgeorigoreuda.store.dto.request.ReportCreateRequest;
+import com.pd.gilgeorigoreuda.store.exception.NoOwnerMemberException;
+import com.pd.gilgeorigoreuda.store.exception.NoRepoterMemberException;
+import com.pd.gilgeorigoreuda.store.exception.NoSuchStoreException;
 import com.pd.gilgeorigoreuda.store.repository.StoreReportHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StoreReportService {
 
     private final StoreReportHistoryRepository storeReportRepository;
 
+    @Transactional
     public void addStoreReport(ReportCreateRequest reportCreateRequest, Long storeId,
                                 Long memberId) {
         storeReportRepository.save(reportCreateRequest.toEntity(storeId, memberId));
     }
+
+    @Transactional
+    public void deleteReport(final Long reportId, final Long memberId) {
+        StoreReportHistory reportForDelete = findReportWithMember(reportId);
+
+        if (!reportForDelete.isRepoter(memberId)) {
+            throw new NoRepoterMemberException();
+        }
+        storeReportRepository.deleteById(reportForDelete.getId());
+    }
+
+    private StoreReportHistory findReportWithMember(Long reportId) {
+        return storeReportRepository.findReportWithMember(reportId)
+                .orElseThrow(NoSuchStoreException::new);
+    }
+
 }
