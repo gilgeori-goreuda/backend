7d764313f52a7bd71d6f9e8ead912fa9d220fb29
alsensto <jmg1301@gmail.com>
2023-11-14 15:49:19 +0900

    feature/#120 Batch 설정 파일 생성

--- src/main/java/com/pd/gilgeorigoreuda/GilgeoriGoreudaApplication.java
+++ src/main/java/com/pd/gilgeorigoreuda/GilgeoriGoreudaApplication.java
@@ -1,16 +1,13 @@
 package com.pd.gilgeorigoreuda;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-@EnableBatchProcessing
 public class GilgeoriGoreudaApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/pd/gilgeorigoreuda/common/config/batch/BatchConfig.java
@@ -0,0 +1,9 @@
+package com.pd.gilgeorigoreuda.common.config.batch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+}
