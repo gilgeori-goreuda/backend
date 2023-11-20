7c37380d8add87b9a0e4792fd12dd526c016649e
Sudongk <aa88sd@naver.com>
2023-11-20 18:36:05 +0900

    feature/#172 SearchParamsArgument 빈 등록 후 ArgumentResolvers에 추가

--- src/main/java/com/pd/gilgeorigoreuda/common/config/web/ResolverConfig.java
+++ src/main/java/com/pd/gilgeorigoreuda/common/config/web/ResolverConfig.java
@@ -1,6 +1,7 @@
 package com.pd.gilgeorigoreuda.common.config.web;
 
 import com.pd.gilgeorigoreuda.auth.resolver.MemberInfoArgumentResolver;
+import com.pd.gilgeorigoreuda.search.resolver.SearchParamsArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,10 +14,12 @@ import java.util.List;
 public class ResolverConfig implements WebMvcConfigurer {
 
     private final MemberInfoArgumentResolver memberInfoArgumentResolver;
+    private final SearchParamsArgumentResolver searchParamsArgumentResolver;
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(memberInfoArgumentResolver);
+        resolvers.add(searchParamsArgumentResolver);
     }
 
 }
---
+++ src/main/java/com/pd/gilgeorigoreuda/search/resolver/SearchParamsArgumentResolver.java
@@ -0,0 +1,23 @@
+package com.pd.gilgeorigoreuda.search.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class SearchParamsArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return false;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+
+}
