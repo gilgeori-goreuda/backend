3629149d9501f076f3adf3af4bf6b9246d4dd4d4
Sudongk <aa88sd@naver.com>
2023-11-27 15:23:56 +0900

    bug/#270 flyway 수정

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -93,6 +93,15 @@ create table stores
     constraint fk_stores_member_id
         foreign key (member_id) references members (id)
 );
+create table food_categories
+(
+    id        bigint auto_increment
+        primary key,
+    food_type enum ('BUNGEOPPANG', 'DALGONA', 'EGGBREAD', 'FIRED', 'GIMBAP', 'HOTTEOK', 'KKOCHI', 'KUNGOGUMA', 'KUNOKSUSU', 'ODENG', 'SUNDAE', 'TACOYAKI', 'TANGHURU', 'TOAST', 'TTAKONGPPANG', 'TTEOKBOKKI', 'WAFFLE') not null,
+    store_id  bigint                                                                                                                                                                                                    null,
+    constraint fk_food_categories_store_id
+        foreign key (store_id) references stores (id)
+);
 create table reviews
 (
     id            bigint auto_increment
