fd1396ad0dd3205f2ee745c575250dc14a1434aa
alsensto <jmg1301@gmail.com>
2023-11-28 12:39:07 +0900

    loki4j 설정 추가

--- src/main/java/com/pd/gilgeorigoreuda/batch/member/scheduler/MemberLevelScheduler.java
+++ src/main/java/com/pd/gilgeorigoreuda/batch/member/scheduler/MemberLevelScheduler.java
@@ -11,7 +11,7 @@ public class MemberLevelScheduler {
 
     private final MemberLevelJob memberLevelJob;
 
-    @Scheduled(cron ="0 0/1 * * * ?")
+    @Scheduled(cron ="0/30 * * * * ?")
     public void runMemberLevelJob() {
         memberLevelJob.updateMemberLeverJob();
     }
--- src/main/java/com/pd/gilgeorigoreuda/batch/store/scheduler/StoreReportUpdateScheduler.java
+++ src/main/java/com/pd/gilgeorigoreuda/batch/store/scheduler/StoreReportUpdateScheduler.java
@@ -22,7 +22,7 @@ public class StoreReportUpdateScheduler {
     private final JobLauncher jobLauncher;
     private final JobRegistry jobRegistry;
 
-    @Scheduled(cron = "0 0/1 * * * *")
+    @Scheduled(cron = "0 0/4 * * * *")
     public void updateStoreReport() {
         String time = LocalDateTime.now().toString();
 
