fde72e607c6c1f73cfab3283e301b96c8249d37c
Sudongk <aa88sd@naver.com>
2024-01-06 18:40:30 +0900

    refactor/#345 변수명 가독성있게 변경

--- src/main/java/com/pd/gilgeorigoreuda/common/util/DistanceCalculator.java
+++ src/main/java/com/pd/gilgeorigoreuda/common/util/DistanceCalculator.java
@@ -9,31 +9,37 @@ public class DistanceCalculator {
     public static final int ONE_HOUR = 3600;
 
     public static int calculateDistance(
-            final BigDecimal memberLat,
-            final BigDecimal memberLng,
-            final BigDecimal storeLat,
-            final BigDecimal storeLng
+            final BigDecimal startLatitude,
+            final BigDecimal startLongitude,
+            final BigDecimal destinationLatitude,
+            final BigDecimal destinationLongitude
     ) {
-        double lat1 = Math.toRadians(memberLat.doubleValue());
-        double lng1 = Math.toRadians(memberLng.doubleValue());
-        double lat2 = Math.toRadians(storeLat.doubleValue());
-        double lng2 = Math.toRadians(storeLng.doubleValue());
+        // 위도와 경도를 라디안 값으로 변환
+        double startLatitudeToRadian = Math.toRadians(startLatitude.doubleValue());
+        double startLongitudeToRadian = Math.toRadians(startLongitude.doubleValue());
+        double destinationLatitudeToRadian = Math.toRadians(destinationLatitude.doubleValue());
+        double destinationLongitudeToRadian = Math.toRadians(destinationLongitude.doubleValue());
 
-        double dLat = lat2 - lat1;
-        double dLon = lng2 - lng1;
+        // 위도와 경도의 차이를 계산
+        double delLatitude = destinationLatitudeToRadian - startLatitudeToRadian;
+        double delLongitude = destinationLongitudeToRadian - startLongitudeToRadian;
 
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-                   Math.cos(lat1) * Math.cos(lat2) *
-                   Math.sin(dLon/2) * Math.sin(dLon/2);
+        double tempVal = Math.sin(delLatitude / 2) * Math.sin(delLatitude / 2) +
+                Math.cos(startLatitudeToRadian) * Math.cos(destinationLatitudeToRadian) *
+                        Math.sin(delLongitude / 2) * Math.sin(delLongitude / 2);
 
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        // 중심각을 계산
+        double centralAngle = 2 * Math.atan2(Math.sqrt(tempVal), Math.sqrt(1 - tempVal));
 
-        return (int) Math.round(EARTH_RADIUS * c);
+        // 두 지점 사이의 거리를 계산 (지구 반지름과 중심각의 곱)
+        return (int) Math.round(EARTH_RADIUS * centralAngle);
     }
 
     public static int calculateApproximateWalkingTime(final int distance) {
+        // 거리를 보행 속도로 나누어 보행 시간 (초)을 계산
         double walkingTimeSeconds = (double) distance / WALKING_SPEED;
 
+        // 보행 시간 (초)을 시간으로 변환
         return (int) Math.ceil(walkingTimeSeconds / ONE_HOUR);
     }
 
