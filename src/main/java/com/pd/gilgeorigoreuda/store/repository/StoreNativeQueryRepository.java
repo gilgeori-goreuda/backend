d4afac1c2824f160ffa1e80a86b75461370c05fe
Sudongk <aa88sd@naver.com>
2024-01-12 22:48:42 +0900

    refactor/#349 가게 정보 변경 서비스 로직 수정

--- src/main/java/com/pd/gilgeorigoreuda/store/repository/StoreNativeQueryRepository.java
+++ src/main/java/com/pd/gilgeorigoreuda/store/repository/StoreNativeQueryRepository.java
@@ -11,28 +11,27 @@ import com.pd.gilgeorigoreuda.store.domain.entity.Store;
 
 public interface StoreNativeQueryRepository extends JpaRepository<Store, Long> {
 
-	// todo: 데이터 스캔 범위를 줄이기 위해 FROM절에 서브 쿼리로 도로명 주소로 한번 필터링을 해면 더 좋을까?
-	// todo: 주소를 대,중,소로 나눠서 검색하면 더 좋을까?
-
 	@Query(value =
-		"SELECT s.id "
-			+ "FROM stores s "
-			+ "WHERE "
-			+ 	"s.large_address = :largeAddress "
-			+   "AND "
-			+   "s.medium_address = :mediumAddress "
-			+ 	"AND "
-			+ 	"FLOOR(6371 * 1000 * acos("
-			+ 		"cos(radians(:lat)) * cos(radians(s.lat)) * cos(radians(:lng) - radians(s.lng)) +"
-			+ 		"sin(radians(:lat)) * sin(radians(s.lat))"
-			+ 	")"
-			+ ") <= :boundary "
-			+ "LIMIT 1",
-		nativeQuery = true)
-	Optional<Long> isAlreadyExistInBoundary(@Param("lat") final BigDecimal lat,
-											@Param("lng") final BigDecimal lng,
-											@Param("largeAddress") final String largeAddress,
-											@Param("mediumAddress") final String mediumAddress,
-											@Param("boundary") final Integer boundary);
+			"SELECT s.id "
+					+ "FROM stores s "
+					+ "WHERE "
+					+ 	"s.large_address = :largeAddress "
+					+   "AND "
+					+   "s.medium_address = :mediumAddress "
+					+ 	"AND "
+					+ 	"FLOOR(6371 * 1000 * acos("
+					+ 		"cos(radians(:lat)) * cos(radians(s.lat)) * cos(radians(:lng) - radians(s.lng)) +"
+					+ 		"sin(radians(:lat)) * sin(radians(s.lat))"
+					+ 	")"
+					+ ") <= :boundary "
+					+ "LIMIT 1",
+			nativeQuery = true)
+	Optional<Long> isAlreadyExistInBoundary(
+			@Param("lat") final BigDecimal lat,
+			@Param("lng") final BigDecimal lng,
+			@Param("largeAddress") final String largeAddress,
+			@Param("mediumAddress") final String mediumAddress,
+			@Param("boundary") final Integer boundary
+	);
 
 }
